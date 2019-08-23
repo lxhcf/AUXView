683597739925017d581ece00a384b7e2bef4b5fd
lixing <935234901@qq.com>
2019-08-23 09:20:22 +0800

    2019/8/23 清除部分冗余代码

--- app/src/main/java/com/example/auxviewdemo/MainActivity.java
+++ app/src/main/java/com/example/auxviewdemo/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.auxviewdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Intent intent=new Intent(this,InstallLoaction.class);
         Intent intent=new Intent(this,WifiConnection.class);
-
-
-
         startActivity(intent);
     }
 }
